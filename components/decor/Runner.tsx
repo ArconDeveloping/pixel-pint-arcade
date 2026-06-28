817cc314302149bd8b0e999035e5687deaaa940c
Daryna Bilan <rinabkh98@gmail.com>
2026-06-28 21:10:32 +0200

    add sprites #11

--- components/decor/Runner.tsx
+++ components/decor/Runner.tsx
@@ -2,12 +2,7 @@ import styles from "./Runner.module.css";
 
 export const Runner = () => (
   <div className={styles.runner} data-runner aria-hidden="true">
-    <span className={styles.head}></span>
-    <span className={styles.body}></span>
-    <span className={`${styles.arm} ${styles.a1}`}></span>
-    <span className={`${styles.arm} ${styles.a2}`}></span>
-    <span className={`${styles.leg} ${styles.l1}`}></span>
-    <span className={`${styles.leg} ${styles.l2}`}></span>
-    <span className={styles.sword}></span>
+    <span className={styles.sprite}></span>
+    <span className={styles.slash}></span>
   </div>
 );
